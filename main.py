293570ca82e175b520548c147085e9084ff42b02
cscheerer <82324733+cscheerer@users.noreply.github.com>
2023-06-16 14:50:06 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# Simple Addition Calculator
+# by cscheerer on GitHub
+
+print("Only does addition with two numbers.")
+one = input("First number: ")
+two = input("Second number: ")
+one = int(one)
+two = int(two)
+result = one + two
+
+# Makes 9+10 = 21
+
+if result == 19:
+    result = 21
+
+print(f"Your result is: {result}")
